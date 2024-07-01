4aa3e1b30bf557e0967a71ac5f1a460c66f5772e
Benjamin Mayer <benjamin.mayer98@gmx.de>
2024-07-01 10:19:30 +0200

    switch to ignition gazebo

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command, NotSubstitution
+from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, AppendEnvironmentVariable
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -11,6 +11,8 @@ from launch_ros.descriptions import ParameterValue
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    package_name = 'akrobat'
 
     ld.add_action(DeclareLaunchArgument(
         'headless',
@@ -32,27 +34,20 @@ def generate_launch_description():
 
     ld.add_action(IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gazebo.launch.py'
-            ])
+            PathJoinSubstitution([FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py'])
         ),
-        launch_arguments={
-            'gui': NotSubstitution(LaunchConfiguration('headless')),
-            'pause': LaunchConfiguration('start_paused')
-        }.items(),
+        launch_arguments={'gz_args': ['-r -v4 empty.sdf']}.items()
     ))
 
     ld.add_action(IncludeLaunchDescription(
         launch_description_source=PythonLaunchDescriptionSource(
-            launch_file_path=PathJoinSubstitution([FindPackageShare('akrobat'), 'launch', 'include', 'rviz.launch.py'])
+            launch_file_path=PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'include', 'rviz.launch.py'])
         ),
         condition=IfCondition(LaunchConfiguration('rviz'))
     ))
 
     ld.add_action(Node(
-        package='akrobat',
+        package=package_name,
         executable='akrobat',
         output='screen'
     ))
@@ -64,7 +59,7 @@ def generate_launch_description():
         output='screen',
         parameters=[{
             'robot_description': ParameterValue(
-                Command(['xacro ', PathJoinSubstitution([FindPackageShare('akrobat'), 'urdf', 'akrobat.xacro']), ' simulation:=True']),
+                Command(['xacro ', PathJoinSubstitution([FindPackageShare(package_name), 'urdf', 'akrobat.xacro']), ' simulation:=True']),
                 value_type=str
             ),
             'use_sim_time': ParameterValue(True, value_type=bool)
@@ -72,11 +67,11 @@ def generate_launch_description():
     ))
 
     ld.add_action(Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+        package='ros_gz_sim',
+        executable='create',
         arguments=['-topic', 'robot_description',
-                   '-entity', 'akrobat',
-                   '-z', '2'],
+                   '-name', package_name,
+                   '-z', '.2'],
         output='screen'
     ))
 
