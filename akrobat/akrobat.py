7ae6fdd5a1311fd5ad1f40913a68b66839c7c11c
akrobat <1823720@stud.hs-mannheim.de>
2024-07-04 17:17:04 +0200

    started implementing multiple gaits

--- akrobat/akrobat.py
+++ akrobat/akrobat.py
@@ -13,9 +13,10 @@ from math import cos, acos, pi, sin, sqrt, degrees, atan2
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 import tf2_ros
+from rclpy.duration import S_TO_NS
 
 from builtin_interfaces.msg import Duration
-from std_msgs.msg import Float64MultiArray, Header
+from std_msgs.msg import Float64MultiArray, Header, String
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Point, Transform
 from tf2_geometry_msgs import PointStamped
@@ -23,10 +24,10 @@ from sensor_msgs.msg import JointState
 from control_msgs.action import FollowJointTrajectory
 
 class Gait(Enum):
-    T_POSE  = 0
-    TRIPOD  = 1
-    WAVE    = 2
-    RIPPLE  = 3
+    T_POSE  = 't_pose'
+    TRIPOD  = 'tripod'
+    WAVE    = 'wave'
+    RIPPLE  = 'ripple'
 
 
 class Joint(Enum):
@@ -98,12 +99,11 @@ class Coordinate(CoordinateBase):
 
 class Akrobat(Node):
 
-    def __init__(self, node_name: str, publish_frequency_Hz: int = 20, initial_gait: Gait = Gait.TRIPOD) -> None:
+    def __init__(self, node_name: str, initial_gait: Gait = Gait.TRIPOD) -> None:
         super().__init__(node_name)
 
-        self.__publish_frequency_Hz = publish_frequency_Hz
         self.__current_gait = initial_gait
-        self.__gait_base_period_seconds = 4
+        self.__gait_base_period_seconds = 2
         self.__gait_speed_modifier = 1
         self.__robot_frame_name = 'akrobat_link' # TODO get from URDF?
         self.joint_names = [f'm{l.value}{j.value+1}'for l in Leg for j in Joint]
@@ -113,17 +113,22 @@ class Akrobat(Node):
 
         self.__current_positions = None
         '''The most up-to-date positions of the robot joints'''
-        self.__goal_positions = [0.] * 3 * Leg.amount()
-        '''The desired positions of the robot joints'''
+
+        self.__default_z = -.145
+        '''default z position of foot relative to akrobat body'''
+        self.__default_y = .19
+        '''default y position of foot relative to akrobat body'''
 
         self.__gait_toggle = True
         self.__gait_granularity = 3
 
+
         self.create_subscription(JointState, 'joint_states', self.__update_positions, 10)
-        self.__run_timer = self.create_timer(self.__gait_base_period_seconds/2, self.tripod)
-        self.create_subscription(PointStamped, 'goal_point', self.__set_goal_point, 10)
+        self.create_subscription(String, '/gait', self.change_gait, 10)
+        # self.__run_timer = self.create_timer(self.gait_period_seconds/2, self.run)
 
         self.__joint_trajectory_publisher = self.create_publisher(JointTrajectory, '/joint_trajectory_controller/joint_trajectory', 10)
+        self.lay_down()
 
     @property
     def gait_period_seconds(self):
@@ -140,8 +145,8 @@ class Akrobat(Node):
     def gait_speed_modifier(self, value):
         # TODO handle value == 0
         self.__gait_speed_modifier = value
-        period_sec = self.__gait_period_time / self.__gait_speed_modifier
-        period_ns = period_sec / (1000**3)
+        period_sec = self.gait_period_seconds/2
+        period_ns = period_sec / S_TO_NS
         self.__run_timer.timer_period_ns = period_ns
 
     def __update_positions(self, joint_state: JointState):
@@ -185,32 +190,31 @@ class Akrobat(Node):
             self.get_logger().error(f'math domain error during inverse kinematics calculation with {point=}')
             return .0,.0,.0
 
-    def __set_goal_point(self, msg: PointStamped):
-        if msg.header.frame_id == self.__robot_frame_name:
-            self.set_leg_goal_coordiante(Leg.RIGHT_REAR, msg)
-        else:
-            for leg in Leg:
-                if leg.value == int(msg.header.frame_id[0]):
-                    self.set_leg_goal_coordiante(leg, msg)
-
-    def set_leg_goal_coordiante(self, leg: Leg, point_stamped: PointStamped) -> None:
+    def coordinate_transform(self, source_point: PointStamped, target_frame: str) -> PointStamped:
         '''
-        Calculates the joint angles for the given coordinate and leg using inverse kinematics.
-        Sets the joint angles as goal positions for the next publish.
+        Calculates the position of the given point in the target_frame's coordinate system.
         '''
-        target_frame = leg.frame_id(Link.Coxa)
 
-        self.get_logger().debug(f'requested point {point_stamped.point} in frame {point_stamped.header.frame_id}')
+        point_stamped = self.__tf_buffer.transform_full(
+            object_stamped=point_stamped,
+            target_frame=target_frame,
+            target_time=tf2_ros.Time(),
+            fixed_frame=source_point.header.frame_id,
+            timeout=rclpy.duration.Duration(seconds=.5))
+        
+        return point_stamped
 
-        point_stamped = self.__tf_buffer.transform_full(point_stamped, target_frame, tf2_ros.Time(), point_stamped.header.frame_id, rclpy.duration.Duration(seconds=.5))
+    def change_gait(self, msg: String):
+        self.__current_gait = Gait(msg.data)
+        self.get_logger().info(f'switched gait to {self.__current_gait}')
 
-        self.get_logger().debug(f'requested point translated to frame {point_stamped.header.frame_id}: {point_stamped.point}')
+    def t_pose(self):
+        trajectory = JointTrajectory()
+        trajectory.joint_names = self.joint_names
+        point = JointTrajectoryPoint(time_from_start=Duration(), positions=[0.]*Leg.amount()*Joint.amount_per_leg())
+        trajectory.points = [point]
 
-        alpha, beta, gamma = self.__inverse_kinematics(point_stamped.point, leg)
-
-        self.__goal_positions[leg.joint_index(Joint.alpha)] = alpha
-        self.__goal_positions[leg.joint_index(Joint.beta)]  = beta
-        self.__goal_positions[leg.joint_index(Joint.gamma)] = gamma
+        self.__joint_trajectory_publisher.publish(trajectory)
 
     def tripod(self):
         # TODO add direction parameter so that tripod can walk in x, -x, y and -y
@@ -228,15 +232,15 @@ class Akrobat(Node):
 
             for leg in Leg:
 
-                y = .19
+                y = self.__default_y
                 is_left_leg = leg.value%2 == 1
                 
                 if leg in return_stroke:
                     x = .05 * -cos(pi * time_step / self.__gait_granularity)
-                    z = -.145 + .06 * sin(pi * time_step / self.__gait_granularity)
+                    z = self.__default_z + .06 * sin(pi * time_step / self.__gait_granularity)
                 else:
                     x = .05 - 2 * .05 * time_step / self.__gait_granularity
-                    z = -.145
+                    z = self.__default_z
 
                 alpha, beta, gamma = self.__inverse_kinematics(Point(x=x if is_left_leg else -x, y=y, z=z))
 
@@ -254,28 +258,71 @@ class Akrobat(Node):
 
         for time_step in range(self.__gait_granularity-1):
             point = calculate_point(time_step)
-            point.time_from_start = Duration(nanosec = int((time_step/self.__gait_granularity) * (self.gait_period_seconds/2 * 1000**3)))
+            point.time_from_start = Duration(nanosec = int((time_step/self.__gait_granularity) * (self.gait_period_seconds/2 * S_TO_NS)))
             trajectory.points.append(point)
 
         self.__gait_toggle = not self.__gait_toggle
 
         # transition to next iteration
         point = calculate_point(0)
-        point.time_from_start = Duration(nanosec= int(self.gait_period_seconds/2 * 1000**3))
+        point.time_from_start = Duration(nanosec= int(self.gait_period_seconds/2 * S_TO_NS))
         trajectory.points.append(point)
 
         self.__joint_trajectory_publisher.publish(trajectory)
 
+    def wave(self):
+        raise NotImplementedError
+    
+    def ripple(self):
+        raise NotImplementedError
+
+    def run(self):
+        if self.__current_gait is Gait.T_POSE:
+            self.t_pose()
+        elif self.__current_gait is Gait.TRIPOD:
+            self.tripod()
+        elif self.__current_gait is Gait.WAVE:
+            self.wave()
+        elif self.__current_gait is Gait.RIPPLE:
+            self.ripple()
+        
+    def lay_down(self):
+        
+        stand_trajectory_point = JointTrajectoryPoint()
+        stand_trajectory_point.time_from_start = Duration(nanosec = int(.5 * S_TO_NS))
+        lay_trajectory_point = JointTrajectoryPoint()
+        lay_trajectory_point.time_from_start = Duration(nanosec = int(1.5 * S_TO_NS))
+        stretch_trajectory_point = JointTrajectoryPoint()
+        stretch_trajectory_point.time_from_start = Duration(nanosec = int(2.5 * S_TO_NS))
+        stretch_trajectory_point.positions = [0.] * Leg.amount() * Joint.amount_per_leg()
+
+        for leg in Leg:
+            stand_point = Point(x=0., y=self.__default_y, z=self.__default_z)
+            stand_trajectory_point.positions.extend(self.__inverse_kinematics(stand_point))
+
+            lay_point = Point(x=0., y=self.__default_y, z=0.)
+            lay_trajectory_point.positions.extend(self.__inverse_kinematics(lay_point))
+
+        trajectory = JointTrajectory()
+        trajectory.points = [stand_trajectory_point, lay_trajectory_point, stretch_trajectory_point]
+        trajectory.joint_names = self.joint_names
+
+        self.__joint_trajectory_publisher.publish(trajectory)
+
+    def shutdown(self):
+        self.__run_timer.cancel()
+        self.lay_down()
 
 def main(args=None):
     rclpy.init(args=args)
 
     akrobat = Akrobat(node_name='akrobat', initial_gait=Gait.TRIPOD)
-
-    rclpy.spin(akrobat)
-
-    akrobat.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(akrobat)
+    finally:
+        akrobat.shutdown()
+        akrobat.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
